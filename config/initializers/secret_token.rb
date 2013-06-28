70d52754774eb08fa5c09bc9ccea53f43292ccbf
Adam Daigian <adam.daigian@gmail.com>
2013-06-28 10:09:09 -0700

    created new playlistr app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Playlistr::Application.config.secret_token = '87a14091027e1b63abd3252600c9d3df10bcb3b1b538e78f9e86f345987a2882dd458685bc062d2c5a42f6d5795f5fa1400343af7c86af9e4663ee3c5df09fb1'
