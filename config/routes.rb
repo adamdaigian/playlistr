ce983864b4e7dcd1db1fd3758e558202fef47b7b
Adam Daigian <adam.daigian@gmail.com>
2013-06-28 10:33:08 -0700

    added home view. and added route to routes file.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Playlistr::Application.routes.draw do
+  get 'playlists' => 'playlists#home'
   get 'playlists/show' => 'playlists#show', :as => "playlist"
   get 'playlists/new' => 'playlists#new', :as => "new_playlist"
-  post 'playlists/create' => 'playlist#create', :as => "create_playlist"
+  post 'playlists' => 'playlist#create', :as => "create_playlist"
+
+  root :to => 'playlists#home'
 end
