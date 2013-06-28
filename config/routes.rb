3bb3eb228a74ac0aa22263df2f0f623f370c161b
Adam Daigian <adam.daigian@gmail.com>
2013-06-28 10:29:56 -0700

    added playlist create route to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Playlistr::Application.routes.draw do
   get 'playlists/show' => 'playlists#show', :as => "playlist"
   get 'playlists/new' => 'playlists#new', :as => "new_playlist"
+  post 'playlists/creare' => 'playlist#create', :as => "create_playlist"
 end
